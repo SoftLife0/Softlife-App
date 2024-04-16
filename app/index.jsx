2d03688f6ffd18c5f4c1824c86447a9409bdd01c
SoftLife0 <onikosiadewale18@gmail.com>
2024-04-16 14:32:16 +0000

    creating my first application with RNative

--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Link } from 'expo-router';
-import profile from './profile';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-white">
       <Text>Hi Softlife, It time to get hungry !!</Text>
-      <Link href="/profile" style={{color:'blue'}}>Go to Profile</Link>
       <StatusBar style="auto" />
+      <Link href="/profile" style={{color:'blue'}}>Go to Profile</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
