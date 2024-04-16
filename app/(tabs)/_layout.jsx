d909536821dfb867b36730cf224624b1baa5cb68
SoftLife0 <onikosiadewale18@gmail.com>
2024-04-16 16:19:39 +0000

    done with Tabs Layout

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -4,8 +4,9 @@ import { icons } from '../../constants'
 
 const TabIcon = ({ icon, color, name, focused }) => {
   return (
-    <View>
+    <View className="items-center justify-center gap-2">
       <Image source={icon} resizeMode="contain" tintColor={color} className="w-6 h-6"/>
+      <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`} style={{color: color}}> {name}</Text>
     </View>
   )
 }
@@ -13,8 +14,20 @@ const TabIcon = ({ icon, color, name, focused }) => {
 const TabsLayout = () => {
   return (
     <>
-        <Tabs>
-            <Tabs.Screen name="home" 
+      <Tabs 
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: '#FFA001',
+          tabBarInactiveTintColor: '#CDCDE0',
+          tabBarStyle: {
+            backgroundColor: '#161622',
+            borderTopWidth: 1,
+            borderTopColor: '#232533',
+            height: 84,
+          }
+          }} >
+          <Tabs.Screen 
+            name="home" 
             options={{
               title: 'Home',
               headerShown: false,
@@ -22,8 +35,45 @@ const TabsLayout = () => {
                 <TabIcon icon={icons.home}
                 color={color} name="Home" focused={focused}/>
               )
-            }}/>
-        </Tabs>
+            }}
+          />
+          
+          <Tabs.Screen 
+            name="bookmark" 
+            options={{
+              title: 'Bookmark',
+              headerShown: false,
+              tabBarIcon: ({ color, focused }) => (
+                <TabIcon icon={icons.bookmark}
+                color={color} name="Bookmark" focused={focused}/>
+              )
+            }}
+          />
+
+          <Tabs.Screen 
+            name="create" 
+            options={{
+              title: 'Create',
+              headerShown: false,
+              tabBarIcon: ({ color, focused }) => (
+                <TabIcon icon={icons.plus}
+                color={color} name="Create" focused={focused}/>
+              )
+            }}
+          />
+
+          <Tabs.Screen 
+            name="profile" 
+            options={{
+              title: 'Profile',
+              headerShown: false,
+              tabBarIcon: ({ color, focused }) => (
+                <TabIcon icon={icons.profile}
+                color={color} name="Profile" focused={focused}/>
+              )
+            }}
+          />
+      </Tabs>
     </>
   )
 }
--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,14 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { View, Text } from 'react-native'
 import React from 'react'
 
-const profile = () => {
+const Profile = () => {
   return (
     <View>
-      <Text>profile</Text>
+      <Text>Profile</Text>
     </View>
   )
 }
 
-export default profile
-
-const styles = StyleSheet.create({})
+export default Profile
