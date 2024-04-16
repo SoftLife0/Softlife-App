2b64e163c878d3cc50c66b2a7ee3e74869d3116e
SoftLife0 <onikosiadewale18@gmail.com>
2024-04-16 18:38:30 +0000

    Done with welcome page i learnt a lot

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { ScrollView, Text, View, Image } from 'react-native';
+import { Redirect, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { images } from '../constants';
-import CustomButton from '../componenets/CustomButton';
+import CustomButton from '../components/CustomButton'
 
 export default function App() {
   return (
-    <SafeAreaView className="bg-secondary h-full">
+    <SafeAreaView className="bg-primary h-full">
         <ScrollView contentContainerStyle={{ height: '100%'}}>
-            <View className="w-full justify-center items-center h-full px-4">
+            <View className="w-full justify-center items-center min-h-[85vh] px-4">
                 <Image source={images.logo} className="w-[130px] h-[84px]" resizeMode="contain"/>
                 <Image source={images.cards} className="max-w-[380px] w-full h-[300px]" resizeMode="contain"/>
 
@@ -23,9 +24,12 @@ export default function App() {
 
                 <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where Creativity Meets Innovation: Embark on a Journey of Limitless Exploration with Aora</Text>
 
-                <CustomButton />
+                <CustomButton title="Continue with Email" handlePress={() => router.push('/signup')} containerStyles="w-full mt-7"/>
+                
             </View>
         </ScrollView>
+
+        <StatusBar backgroundColor='#161622' style='light' />
     </SafeAreaView>
   );
 }
